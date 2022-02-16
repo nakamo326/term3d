af1cc9ca0eb7ce6a28b73add5aeb109a91171062
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-16 13:44:44 +0900

    画面クリアを初期化時のみに変更

--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,13 @@
 #include "term3d.h"
 
 void rotate_loop_tmp(t_point *list, char **canvas) {
+	printf("\033[2J");
 	while (1) {
 		// char c;
 		// c = getchar();
 		// if (c == 'q')
 		// 	break;
 		usleep(60000);
-		printf("\033[2J");
 		printf("\033[%d;%dH" ,1, 1);
 		init_canvas(canvas);
 		rotate_object(list);
