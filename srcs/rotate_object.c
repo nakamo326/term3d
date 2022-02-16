386acb6aab2b87a0f43088f4a413a561e3c4d89f
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-16 13:21:25 +0900

    x軸、z軸の回転追加

--- srcs/rotate_object.c
+++ srcs/rotate_object.c
@@ -6,7 +6,17 @@
 // https://imagingsolution.net/math/rotation-scaling-translation-3d-matrix/
 
 
-void rotate_point(t_point *point, double angle) {
+void rotate_point_x_axis(t_point *point, double angle) {
+	t_point tmp;
+	tmp.x_pos = point->x_pos;
+	tmp.y_pos = cos(angle) * point->y_pos - sin(angle) * point->z_pos;
+	tmp.z_pos = sin(angle) * point->y_pos + cos(angle) * point->z_pos;
+	point->x_pos = tmp.x_pos;
+	point->y_pos = tmp.y_pos;
+	point->z_pos = tmp.z_pos;
+}
+
+void rotate_point_y_axis(t_point *point, double angle) {
 	t_point tmp;
 	tmp.x_pos = cos(angle) * point->x_pos + sin(angle) * point->z_pos;
 	tmp.y_pos = point->y_pos;
@@ -16,11 +26,21 @@ void rotate_point(t_point *point, double angle) {
 	point->z_pos = tmp.z_pos;
 }
 
+void rotate_point_z_axis(t_point *point, double angle) {
+	t_point tmp;
+	tmp.x_pos = cos(angle) * point->x_pos - sin(angle) * point->y_pos;
+	tmp.y_pos = sin(angle) * point->x_pos + cos(angle) * point->y_pos;
+	tmp.z_pos = point->z_pos;
+	point->x_pos = tmp.x_pos;
+	point->y_pos = tmp.y_pos;
+	point->z_pos = tmp.z_pos;
+}
+
 void rotate_object(t_point *list) {
 	t_point *point;
 	point = list;
 	while(point) {
-		rotate_point(point, M_PI / 180);
+		rotate_point_y_axis(point, M_PI / 180);
 		point = point->next;
 	}
 }
