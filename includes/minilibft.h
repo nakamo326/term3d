eddaa4561d5f3c6ce36299a992a6e499f09a6d5e
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-17 12:02:39 +0900

    headerのnormエラー対応

--- includes/minilibft.h
+++ includes/minilibft.h
@@ -1,21 +1,19 @@
 #ifndef MINILIBFT_H
 # define MINILIBFT_H
 
-#include "stdlib.h"
-#include "unistd.h"
+# include "stdlib.h"
+# include "unistd.h"
 
 # define GNL_BUFFER_SIZE 1024
-
 
 typedef enum e_bool
 {
 	FALSE,
 	TRUE
-}				t_bool;
-
+}	t_bool;
 
 char	*ft_find_new_line(const char *s, size_t len);
-int	ft_get_next_line(int fd, char **line);
+int		ft_get_next_line(int fd, char **line);
 void	ft_safe_free_split(char ***target);
 char	**ft_split(char const *s, char c);
 void	ft_safe_free_char(char **target);
