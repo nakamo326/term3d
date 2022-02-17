6bbdd3c2526505cc2e26cd30690c9b861927d076
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-17 12:50:49 +0900

    norm修正

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,17 +1,17 @@
 #include "term3d.h"
 
-int calc_pos_in_canvas(double pos, int dim_size, int canvas_size)
+int	calc_pos_in_canvas(double pos, int dim_size, int canvas_size)
 {
-	double dimension_pos;
-	double canvas_pos;
+	double	dimension_pos;
+	double	canvas_pos;
 
 	pos += (dim_size / 2);
 	dimension_pos = pos / dim_size;
 	canvas_pos = round(dimension_pos * canvas_size);
-	return (int)(canvas_pos);
+	return ((int)canvas_pos);
 }
 
-bool is_drawable_point(int pos, int canvas_size)
+bool	is_drawable_point(int pos, int canvas_size)
 {
 	if (pos >= 0 && pos < canvas_size)
 	{
@@ -20,10 +20,10 @@ bool is_drawable_point(int pos, int canvas_size)
 	return (false);
 }
 
-void draw_point(t_point *point, char **canvas)
+void	draw_point(t_point *point, char **canvas)
 {
-	int x_pos;
-	int y_pos;
+	int	x_pos;
+	int	y_pos;
 
 	x_pos = calc_pos_in_canvas(point->x_pos, DIM_X_SIZE, CANVAS_X_SIZE);
 	y_pos = calc_pos_in_canvas(point->y_pos, DIM_Y_SIZE, CANVAS_Y_SIZE);
@@ -34,9 +34,9 @@ void draw_point(t_point *point, char **canvas)
 	}
 }
 
-void draw_object(t_point *point_list, char **canvas)
+void	draw_object(t_point *point_list, char **canvas)
 {
-	t_point *point;
+	t_point	*point;
 
 	point = point_list;
 	while (point)
