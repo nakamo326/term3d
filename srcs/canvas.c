a58e92d7e6222f77f45c82fab779c0349527ae71
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-17 12:34:32 +0900

    fix norm error

--- srcs/canvas.c
+++ srcs/canvas.c
@@ -1,32 +1,32 @@
 #include "term3d.h"
 
-void init_canvas(char** canvas)
+void	init_canvas(char **canvas)
 {
-	size_t index;
+	size_t	index;
 
 	index = 0;
-	while(canvas[index])
+	while (canvas[index])
 	{
 		memset(canvas[index], ' ', (sizeof(char) * CANVAS_X_SIZE));
 		index++;
 	}
 }
 
-char **allocate_canvas(void)
+char	**allocate_canvas(void)
 {
-	char** res;
-	size_t index;
+	char	**res;
+	size_t	index;
 
 	index = 0;
 	res = malloc(sizeof(char *) * (CANVAS_Y_SIZE + 1));
 	if (!res)
-		exit(EXIT_FAILURE); //error
+		exit(EXIT_FAILURE);
 	res[CANVAS_Y_SIZE] = NULL;
-	while(index < CANVAS_Y_SIZE)
+	while (index < CANVAS_Y_SIZE)
 	{
 		res[index] = malloc(sizeof(char) * CANVAS_X_SIZE + 1);
-		if(!res[index])
-			exit(EXIT_FAILURE); //error
+		if (!res[index])
+			exit(EXIT_FAILURE);
 		res[index][CANVAS_X_SIZE] = '\0';
 		index++;
 	}
@@ -34,9 +34,9 @@ char **allocate_canvas(void)
 	return (res);
 }
 
-void print_canvas(char **canvas)
+void	print_canvas(char **canvas)
 {
-	size_t index;
+	size_t	index;
 
 	index = 0;
 	while (canvas[index])
@@ -46,10 +46,10 @@ void print_canvas(char **canvas)
 	}
 }
 
-void free_canvas(char ***canvas)
+void	free_canvas(char ***canvas)
 {
-	size_t index;
-	char** canvas_mem;
+	size_t	index;
+	char	**canvas_mem;
 
 	canvas_mem = *canvas;
 	index = 0;
