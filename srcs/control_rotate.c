215f7d2ec8d140177677cf13bc601873cdb31e9f
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-21 13:44:50 +0900

    回転速度をdefine

--- srcs/control_rotate.c
+++ srcs/control_rotate.c
@@ -6,26 +6,28 @@
 /*   By: ynakamot <ynakamot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 19:28:03 by ynakamot          #+#    #+#             */
-/*   Updated: 2022/02/20 11:45:33 by ynakamot         ###   ########.fr       */
+/*   Updated: 2022/02/21 13:43:42 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "term3d.h"
 
+#define ROTATION_SPEED (M_PI / 180 * 2)
+
 void	rotate_object(t_point *list, char control)
 {
 	static t_index	axis = Y_INDEX;
-	static double	angle = M_PI / 180;
+	static double	rotation_speed = ROTATION_SPEED;
 
 	if (control == 'a' || control == 'd')
 		axis = Y_INDEX;
 	else if (control == 'w' || control == 's')
 		axis = X_INDEX;
 	if (control == 'w' || control == 'd')
-		angle = M_PI / 180;
+		rotation_speed = ROTATION_SPEED;
 	else if (control == 'a' || control == 's')
-		angle = -M_PI / 180;
-	rotate_points(axis, list, 2 * angle);
+		rotation_speed = -ROTATION_SPEED;
+	rotate_points(axis, list, rotation_speed);
 }
 
 void	add_angle(t_angle *angle_state, t_index axis, int degree)
